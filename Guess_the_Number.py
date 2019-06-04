4ebabac2459e417fc5e3ce5d3a35123bb6d2d34e
scottwstine <scottwstine@gmail.com>
2019-06-04 15:05:44 -0700

    reviewed and added notes

--- Guess_the_Number.py
+++ Guess_the_Number.py
@@ -1,8 +1,10 @@
+"""Guess the number with warm/cold hints """
+
 import random
 
 x = random.randint(1, 20)
 count = 0
-last_guess = None
+last_guess = None # see line 16
 while True:
     current_guess = input("Guess a number between 1 and 20.")
     current_guess = int(current_guess)
@@ -11,7 +13,7 @@ while True:
         print(f'Good guess. {x} is correct. That took you {count} attempts!')
         break
 
-    if last_guess is not None:
+    if last_guess is not None: # only runs after there has been a guess. So, not the 1st time through
         last_proximity = abs(last_guess - x)
         new_proximity = abs(current_guess - x)
         if new_proximity < last_proximity:
